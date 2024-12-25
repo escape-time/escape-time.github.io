6227a27e853196c3195d5e6ea72c76c22eb23bec
kuo <=>
2024-12-25 10:32:18 +0900

    경로 수정

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -12,7 +12,7 @@ export const Card = ({ item, onClick }: { item: ITEM_TYPE; onClick: () => void }
     <CardWrap key={item.id} onClick={onClick}>
       <ImageWrapper>
         <LazyLoadImage
-          src={`//assets/theme-img/thumb_${item.id}.jpg`}
+          src={`/assets/theme-img/thumb_${item.id}.jpg`}
           alt={item.title}
           onError={() => handleImageError(item)}
         />
